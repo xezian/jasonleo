105a977d6043fd1a1aa44c9d5224da3240b54fee
Leo, Jason <xezian@icloud.com>
2020-08-14 17:23:00 -0400

    Start Vue app for surface, strip away Vue branding

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Jason Leo](https://www.jasonleo.dev)
+
+Hey, hi!
+I am Jason Leo, and this is my personal website.
---
+++ surface/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TableOfContents :open="true" />
+  </div>
+</template>
+
+<script>
+import TableOfContents from "./components/TableOfContents.vue";
+
+export default {
+  name: "App",
+  components: {
+    TableOfContents,
+  },
+};
+</script>
+
+<style></style>
