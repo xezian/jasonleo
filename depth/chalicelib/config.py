0129a9372d0eea43886dc295f32527122c597472
Leo, Jason <xezian@icloud.com>
2020-08-26 17:42:59 -0400

    Remember how it works, and resolve validation fro region-name

--- depth/chalicelib/config.py
+++ depth/chalicelib/config.py
@@ -5,7 +5,7 @@ if os.environ["CHALICE_ENV"] == "local":
     dynamodb = boto3.client(
         "dynamodb",
         endpoint_url="http://localhost:7777",
-        region_name="dummy_region",
+        region_name="us-dummy-1",
         aws_access_key_id="dummy_access_key",
         aws_secret_access_key="dummy_secret_key",
         verify=False,
@@ -13,7 +13,7 @@ if os.environ["CHALICE_ENV"] == "local":
     resource = boto3.resource(
         "dynamodb",
         endpoint_url="http://localhost:7777",
-        region_name="dummy_region",
+        region_name="us-dummy-1",
         aws_access_key_id="dummy_access_key",
         aws_secret_access_key="dummy_secret_key",
         verify=False,
