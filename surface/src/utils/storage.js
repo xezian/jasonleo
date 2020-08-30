1d88b1e6f0b7de97f582311ccd83b7ed3988de35
Leo, Jason <xezian@icloud.com>
2020-08-30 13:31:48 -0400

    Reads a csv with post data in it directly from s3 to the frontend

---
+++ surface/src/utils/storage.js
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { Storage } from "aws-amplify"
+
+const storage = {
+  getBlogPosts: () => {
+    return Storage.get("index.csv", { customPrefix: { public: "blog/" } })
+      .then(async (response) => {
+        const posts = await readInPosts(response)
+        return [null, posts]
+      })
+      .catch((err) => {
+        console.error(err)
+        return [err, null]
+      })
+  },
+}
+
+const readInPosts = (postCsv) => {
+  return axios
+    .get(postCsv)
+    .then((response) => {
+      const lines = response.data.split(/\r\n|\n/)
+      const posts = []
+      for (let i = 1; i < lines.length; i++) {
+        const commaseps = lines[i].split(/,|"/)
+        posts.push({
+          title: commaseps[0],
+          description: commaseps[1],
+          date: commaseps[2],
+          slug: commaseps[3],
+          tags: commaseps.slice(5, commaseps.length - 1).join(),
+        })
+      }
+      return posts
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+export default storage
